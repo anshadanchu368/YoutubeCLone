0386b7d8f5dd163e496e8f5d601df07ef5156bf7
anshad <anshadanchu368@example.com>
2023-06-03 20:35:11 +0400

    6th commit

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { VideoStableSharp } from "@mui/icons-material";
+import ChannelCard from "./ChannelCard";
+import Videos from "./Videos";
 
 const ChannelDetail = () => {
-  return (
-    <div>
-      Channel
-    </div>
-  )
-}
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+  const { id } = useParams();
 
-export default ChannelDetail
+  console.log(channelDetail, videos);
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) =>
+      setChannelDetail(data?.items[0])
+    );
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then(
+      (data) => setVideos(data?.items)
+    );
+  }, [id]);
+  return (
+    <Box minHeight="95vh">
+      <Box>
+        <div
+          style={{
+            zIndex: !0,
+            height: "300px",
+            background: "rgb(2,0,36)",
+            background:
+              "linear-gradient(294deg, rgba(2,0,36,1) 0%, rgba(9,20,121,1) 28%, rgba(0,212,255,1) 86%)",
+          }}
+        />
+        <ChannelCard channelDetail={channelDetail} marginTop="-93px" />
+        
+      </Box>
+      <Box display="flex" p="1">
+        <Box sx={{ mr: { sm: "100px" } }} />
+        <Videos videos={videos} />
+      </Box>
+    </Box>
+  );
+};
+
+export default ChannelDetail;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import { Stack } from "@mui/material";
 import { categories } from "../utils/constants";
 
-const Sidebar = ({selectedCategory,setselectedCategory}) => {
+const Sidebar = ({ selectedCategory, setselectedCategory }) => {
   return (
     <Stack
       direction="row"
@@ -14,14 +14,28 @@ const Sidebar = ({selectedCategory,setselectedCategory}) => {
       {categories.map((category) => (
         <button
           className="category-btn"
-          onClick={()=>setselectedCategory(category.name)}
+          onClick={() => setselectedCategory(category.name)}
           style={{
             background: category.name === selectedCategory && "#FC1503",
-            color:"white"
-          }} key={category.name}
+            color: "white",
+          }}
+          key={category.name}
         >
-            <span style={{color: category.name === selectedCategory ? 'white' : 'red' ,marginRight:"15px"}}>{category.icon}</span><span style={{ opacity: category.name === selectedCategory ? '1' : '0.8'}}>{category.name}</span>
-
+          <span
+            style={{
+              color: category.name === selectedCategory ? "white" : "red",
+              marginRight: "15px",
+            }}
+          >
+            {category.icon}
+          </span>
+          <span
+            style={{
+              opacity: category.name === selectedCategory ? "1" : "0.8",
+            }}
+          >
+            {category.name}
+          </span>
         </button>
       ))}
     </Stack>
